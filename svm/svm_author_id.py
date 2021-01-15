cee6edbcafb8c53b0dc0218559c9f909f8a1dd28
zhwenyu <wenyu_zhang@brown.edu>
2021-01-15 12:56:26 -0500

    svm exer

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -19,12 +19,26 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
+# features_train = features_train[:len(features_train)/100]
+# labels_train = labels_train[:len(labels_train)/100]
 
 
 #########################################################
 ### your code goes here ###
+from sklearn.svm import SVC
+clf = SVC(kernel = 'rbf', C = 10000.)
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
 
+t0 = time()
+pred = clf.predict(features_test)
+print "predicting time:", round(time()-t0, 3), "s"
+# print pred[10], pred[26], pred[50]
+import numpy as np 
+print np.count_nonzero(pred == 1)
+# acc = clf.score(features_test, labels_test)
+# print "accuracy ", acc 
 #########################################################
 
 

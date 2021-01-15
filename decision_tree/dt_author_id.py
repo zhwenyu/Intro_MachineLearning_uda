3515dd7207e0569bf3f1c35b4209fa35317bc924
zhwenyu <wenyu_zhang@brown.edu>
2021-01-15 14:29:23 -0500

    ddt exer

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -19,13 +19,17 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
+print len(features_train[0])
 
 
 #########################################################
 ### your code goes here ###
+from sklearn import tree
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+clf.fit(features_train, labels_train)
 
-
+acc = clf.score(features_test, labels_test)
+print "acc = ", acc
 #########################################################
 
 

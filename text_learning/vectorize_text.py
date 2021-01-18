7bc39616a82c4d008444e242c94bc3a1e206e62d
zhwenyu <wenyu_zhang@brown.edu>
2021-01-18 16:25:53 -0500

    text learning

--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -41,21 +41,25 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
     for path in from_person:
         ### only look at first 200 emails when developing
         ### once everything is working, remove this line to run over full dataset
-        temp_counter += 1
-        if temp_counter < 200:
+            temp_counter += 1
+        # if temp_counter < 200: 
             path = os.path.join('..', path[:-1])
-            print path
+            # print path
             email = open(path, "r")
 
             ### use parseOutText to extract the text from the opened email
-
+            texts = parseOutText(email)
             ### use str.replace() to remove any instances of the words
             ### ["sara", "shackleton", "chris", "germani"]
-
+            for w in ["sara", "shackleton", "chris", "germani"]:
+                texts.replace(w, "")
             ### append the text to word_data
-
+            word_data.append(texts)
             ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
-
+            if name == "sara":
+                from_data.append(0)
+            else:
+                from_data.append(1)
 
             email.close()
 
@@ -66,10 +70,14 @@ from_chris.close()
 pickle.dump( word_data, open("your_word_data.pkl", "w") )
 pickle.dump( from_data, open("your_email_authors.pkl", "w") )
 
-
+print "printing ", word_data[152]
 
 
 
 ### in Part 4, do TfIdf vectorization here
-
-
+from sklearn.feature_extraction.text import TfidfVectorizer
+vectorizer = TfidfVectorizer(stop_words='english')
+word_trans = vectorizer.fit_transform(word_data)
+mapping = vectorizer.get_feature_names()
+print len(mapping)
+print mapping[34597]
--- tools/parse_out_email_text.py
+++ tools/parse_out_email_text.py
@@ -28,15 +28,19 @@ def parseOutText(f):
         text_string = content[1].translate(string.maketrans("", ""), string.punctuation)
 
         ### project part 2: comment out the line below
-        words = text_string
+        # words = text_string
 
         ### split the text string into individual words, stem each word,
         ### and append the stemmed word to words (make sure there's a single
         ### space between each stemmed word)
-        
-
-
-
+     
+        from nltk.stem.snowball import SnowballStemmer
+        stemmer = SnowballStemmer("english")
+        text_string = text_string.split()
+        # print text_string
+        for t in text_string:
+            words += stemmer.stem(t)
+            words += " "
 
     return words
 
